ac81fc51a82ea356c1db409fa938cd6f2d536b15
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-10-24 14:11:16 -0300

    subindo atualização

--- demo/src/main/java/org/example/core/port/PaymentRepository.java
+++ demo/src/main/java/org/example/core/port/PaymentRepository.java
@@ -1,10 +1,14 @@
 package org.example.core.port;
 
 
+import org.example.core.domain.model.Card;
 import org.example.core.domain.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment,String> {
+    Optional<Payment> findByExternalIdPayment(String externalIdPayment);
 }
--- demo/src/main/java/org/example/core/useCase/debts/SaveDebtsUseCase.java
+++ demo/src/main/java/org/example/core/useCase/debts/SaveDebtsUseCase.java
@@ -1,12 +1,14 @@
 package org.example.core.useCase.debts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.core.domain.model.Payment;
 import org.example.core.domain.model.dto.requestDto.DebtsRequest;
 import org.example.core.domain.excecao.NoItemException;
 import org.example.core.domain.model.Card;
 import org.example.core.domain.model.Debts;
 import org.example.core.port.CardRepository;
 import org.example.core.port.DebtsRepository;
+import org.example.core.port.PaymentRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class SaveDebtsUseCase {
                 private final CardRepository cardRepository;
                 private final DebtsRepository debtsRepository;
+                private final PaymentRepository paymentRepository;
         public Debts save (DebtsRequest debtsRequest){
                 Card card = cardRepository.findByExternalIdCard(debtsRequest.getIdexternoCard()).orElseThrow(()->
                         new NoItemException("Cartão não encontrado"));
